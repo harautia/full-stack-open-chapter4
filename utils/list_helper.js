71efdbd4ba4536bf4b2af92232a1e527f0aaef94
harautia <harautia1976@gmail.com>
2025-06-29 00:22:44 +0300

    Excercise 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,6 +3,36 @@ const dummy = (blogs) => {
   return 1
 }
 
+const sumOfLikes =(blogs) => {
+
+// Basic for loop approach
+/*  for (let i = 0; i < blogs.length; i++) {
+    let = likeAmount = blogs[i].likes
+    console.log(likeAmount)
+    }
+*/
+
+// Functional way of doing with forEach
+/*
+let totalLikes = 0
+blogs.forEach(function(blog) {
+    totalLikes = blog.likes + totalLikes
+})
+*/
+
+// Functional way of doing with Reduce 
+
+/* var totalLikes = blogs.reduce (function(sum, blogs){
+  return sum + blogs.likes
+ }, 0)
+*/
+
+// The same function as above defined like this:
+var totalLikes = blogs.reduce ((sum, blogs) => sum + blogs.likes,0)
+
+return totalLikes
+}  
+
 module.exports = {
-  dummy
+  dummy, sumOfLikes
 }
